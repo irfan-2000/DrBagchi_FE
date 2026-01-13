af00cdfde5340aa626629b5a570c9f31d62d1b94
irfan-2000 <irfan201819@gmail.com>
2026-01-13 08:26:46 +0530

    irfan

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,10 +1,13 @@
 export const environment = {
     production: false,
 
-  // baseUrl: 'https://api.drbagchisclasses.com/',
-    baseUrl: 'http://localhost:8080/',
+   baseUrl: 'https://api.drbagchisclasses.com/',
+   // baseUrl: 'http://localhost:8080/',
 
 // baseUrl: 'https://admin.api.race-elearn.com:444/'
     // baseUrl:'https://api.race-elearn.com/'
-admin_baseurl:'https://localhost:7091/'  
+//admin_baseurl:'https://localhost:7091/'  
+
+admin_baseurl:'https://admin.api.drbagchisclasses.com/'
+
 };
--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -2,9 +2,9 @@ export const environment =
 {
   production: false,
    //baseUrl: 'https://api.drbagchisclasses.com/',
-   baseUrl: 'http://localhost:8080/',
+   baseUrl: 'https://api.drbagchisclasses.com/',
 //http://localhost:8080/api/GetAvailableBoards
-   admin_baseurl:'https://localhost:7091/',
+   admin_baseurl:'https://admin.api.drbagchisclasses.com/',
 
 razorpay_key_id:'rzp_test_RnaYCAihA9jZ2M',
 razorpay_key_secret:'MXr2AXE6gdVG3OQiG0R5fSYW'
